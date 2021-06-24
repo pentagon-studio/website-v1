40994c867d8d108dd6b1d9b6ad20b9bb426a7116
Amal <howareyouami@gmail.com>
2021-06-24 20:43:05 +0530

    Add custom font

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,12 @@ export default function Home() {
       <Head>
         <title>Pentagon Studio</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
@@ -16,7 +22,7 @@ export default function Home() {
           width={300}
           height={300}
         />
-        <p className="mt-3 text-2xl font-mono">
+        <p className="mt-10 text-4xl font-comfortaa">
           Building digital products that <br />
           <span className="text-green-600">loves users</span>
         </p>
